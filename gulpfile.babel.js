dc4d95b204697a6459829dfa9eb46e50b8b850db
Furkan Tunali <mail@furkantunali.com>
2021-01-14 15:11:41 +0100

    update resume

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,51 +1,50 @@
-import browserSync from 'browser-sync'
-import del from 'del'
-import fs from 'fs'
-import pdf from 'pdfcrowd'
-import gulp from 'gulp'
-import gulpLoadPlugins from 'gulp-load-plugins'
-import path from 'path'
-import { output as pagespeed } from 'psi'
-import reporters from 'reporters'
-import runSequence from 'run-sequence'
-import swPrecache from 'sw-precache'
-import pkg from './package.json'
-import { exec } from 'child_process'
+import browserSync from "browser-sync";
+import del from "del";
+import fs from "fs";
+import pdf from "pdfcrowd";
+import gulp from "gulp";
+import gulpLoadPlugins from "gulp-load-plugins";
+import path from "path";
+import reporters from "reporters";
+import runSequence from "run-sequence";
+import swPrecache from "sw-precache";
+import pkg from "./package.json";
+import { exec } from "child_process";
 
-const $ = gulpLoadPlugins()
-const reload = browserSync.reload
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
 const jsSource = [
-  './*.js',
-  './**/*.js',
-  'app/scripts/**/*.js',
-  '!app/scripts/**/*.min.js'
-]
+  "./*.js",
+  "./**/*.js",
+  "app/scripts/**/*.js",
+  "!app/scripts/**/*.min.js"
+];
 
 // Load custom tasks from the `tasks` directory
 try {
-  require('require-dir')('tasks')
+  require("require-dir")("tasks");
 } catch (err) {
-  console.error(err)
+  console.error(err);
 }
 
-gulp.task('resume', () =>
+gulp.task("resume", () =>
   gulp
-    .src('app/resume.html')
-    .pipe($.rename('resume-dist.html'))
+    .src("app/resume.html")
+    .pipe($.rename("resume-dist.html"))
     .pipe($.removeCode({ resume: true }))
-    .pipe(gulp.dest('app/'))
-)
+    .pipe(gulp.dest("app/"))
+);
 
-gulp.task('resume-doc', () =>
+gulp.task("resume-doc", () =>
   gulp
-    .src('app/resume.html')
-    .pipe($.rename('resume-doc.html'))
+    .src("app/resume.html")
+    .pipe($.rename("resume-doc.html"))
     .pipe($.removeCode({ resumeDoc: true }))
-    .pipe(gulp.dest('app/'))
-)
+    .pipe(gulp.dest("app/"))
+);
 
 // Lint JavaScript
-gulp.task('prettier-scripts', () =>
+gulp.task("prettier-scripts", () =>
   gulp
     .src(jsSource)
     .pipe(
@@ -56,12 +55,12 @@ gulp.task('prettier-scripts', () =>
       })
     )
     .pipe(gulp.dest(({ base }) => base))
-)
+);
 
 // Optimize images
-gulp.task('images', () =>
+gulp.task("images", () =>
   gulp
-    .src('app/images/**/*')
+    .src("app/images/**/*")
     .pipe(
       $.cache(
         $.imagemin({
@@ -70,105 +69,105 @@ gulp.task('images', () =>
         })
       )
     )
-    .pipe(gulp.dest('dist/images'))
+    .pipe(gulp.dest("dist/images"))
     .pipe(
       $.size({
-        title: 'images'
+        title: "images"
       })
     )
-)
+);
 
 // icons
-gulp.task('icons', () =>
+gulp.task("icons", () =>
   gulp
-    .src('app/icons/**/*')
-    .pipe(gulp.dest('dist/icons'))
+    .src("app/icons/**/*")
+    .pipe(gulp.dest("dist/icons"))
     .pipe(
       $.size({
-        title: 'icons'
+        title: "icons"
       })
     )
-)
+);
 
 // Copy all files at the root level (app)
-gulp.task('copy', () =>
+gulp.task("copy", () =>
   gulp
     .src(
       [
-        'app/*',
-        '!app/*.html',
-        'node_modules/apache-server-configs/dist/.htaccess'
+        "app/*",
+        "!app/*.html",
+        "node_modules/apache-server-configs/dist/.htaccess"
       ],
       {
         dot: true
       }
     )
-    .pipe(gulp.dest('dist'))
-    .pipe($.size({ title: 'copy' }))
-)
+    .pipe(gulp.dest("dist"))
+    .pipe($.size({ title: "copy" }))
+);
 
 // Copy web fonts to dist
-gulp.task('pdf-make', cb => {
+gulp.task("pdf-make", cb => {
   // create an API client instance
   const client = new pdf.HtmlToPdfClient(
-    'JacopKane',
-    '9fe78e65f9e50026ef1236f561b48c3b'
-  )
+    "JacopKane",
+    "9fe78e65f9e50026ef1236f561b48c3b"
+  );
 
   client
-    .setTitle('Furkan Tunali Resume')
-    .setPageSize('A2')
+    .setTitle("Furkan Tunali Resume")
+    .setPageSize("A2")
     .setNoMargins(true)
     .convertUrlToFile(
-      'https://furkantunali.com/resume-doc.html',
-      'Furkan_Tunali_Resume.pdf',
+      "https://furkantunali.com/resume-doc.html",
+      "Furkan_Tunali_Resume.pdf",
       function(error, fileName) {
-        if (error) return cb(error)
-        console.info('Success: the file was created '.concat(fileName))
-        cb()
+        if (error) return cb(error);
+        console.info("Success: the file was created ".concat(fileName));
+        cb();
       }
-    )
-})
+    );
+});
 
-gulp.task('pdf-move', ['pdf-make'], () =>
-  gulp.src(['Furkan_Tunali_Resume.pdf']).pipe(gulp.dest('app/pdf'))
-)
+gulp.task("pdf-move", ["pdf-make"], () =>
+  gulp.src(["Furkan_Tunali_Resume.pdf"]).pipe(gulp.dest("app/pdf"))
+);
 
-gulp.task('pdf-del', cb => del(['Furkan_Tunali_Resume.pdf'], {}, cb))
+gulp.task("pdf-del", cb => del(["Furkan_Tunali_Resume.pdf"], {}, cb));
 
-gulp.task('pdf', ['pdf-move'], () =>
+gulp.task("pdf", ["pdf-move"], () =>
   gulp
-    .src(['app/pdf/**/*.pdf'])
-    .pipe(gulp.dest('dist/pdf'))
+    .src(["app/pdf/**/*.pdf"])
+    .pipe(gulp.dest("dist/pdf"))
     .pipe(
       $.size({
-        title: 'pdf'
+        title: "pdf"
       })
     )
-)
+);
 
 // Copy web fonts to dist
-gulp.task('fonts', () =>
+gulp.task("fonts", () =>
   gulp
-    .src(['app/fonts/**'])
-    .pipe(gulp.dest('dist/fonts'))
-    .pipe($.size({ title: 'fonts' }))
-)
+    .src(["app/fonts/**"])
+    .pipe(gulp.dest("dist/fonts"))
+    .pipe($.size({ title: "fonts" }))
+);
 
-gulp.task('scss-lint-fix', cb => {
-  exec('npm run sass-lint-auto-fix', (error, stdout, stderr) => {
+gulp.task("scss-lint-fix", cb => {
+  exec("npm run sass-lint-auto-fix", (error, stdout, stderr) => {
     if (error) {
-      return cb(error)
+      return cb(error);
     }
-    console.error(stderr)
-    console.log(stdout)
-    cb()
-  })
-})
+    console.error(stderr);
+    console.log(stdout);
+    cb();
+  });
+});
 
-gulp.task('prettier-sass', () =>
+gulp.task("prettier-sass", () =>
   gulp
-    .src('app/styles/**/*.scss')
+    .src("app/styles/**/*.scss")
     .pipe(
       $.prettier({
         singleQuote: true,
@@ -177,76 +176,76 @@ gulp.task('prettier-sass', () =>
       })
     )
     .pipe(gulp.dest(({ base }) => base))
-)
+);
 
-gulp.task('scss-lint', ['prettier-sass', 'scss-lint-fix'], () =>
-  gulp.src('app/styles/**/*.scss').pipe(
+gulp.task("scss-lint", ["prettier-sass", "scss-lint-fix"], () =>
+  gulp.src("app/styles/**/*.scss").pipe(
     $.scssLint({
       bundleExec: true
     })
   )
-)
+);
 
-gulp.task('gulp-css-lint', () =>
+gulp.task("gulp-css-lint", () =>
   gulp
-    .src('app/styles/**/*.css')
+    .src("app/styles/**/*.css")
     .pipe($.csslint())
-    .pipe($.csslint.reporter(reporters('gulp-csslint')))
-)
+    .pipe($.csslint.reporter(reporters("gulp-csslint")))
+);
 
 // Compile and automatically prefix stylesheets
-gulp.task('styles', ['scss-lint'], () =>
+gulp.task("styles", ["scss-lint"], () =>
   gulp
-    .src('app/styles/**/*.scss')
+    .src("app/styles/**/*.scss")
     .pipe(
-      $.changed('.tmp/styles', {
-        extension: '.css'
+      $.changed(".tmp/styles", {
+        extension: ".css"
       })
     )
     .pipe($.sourcemaps.init())
     .pipe(
       $.sass({
         precision: 10
-      }).on('error', reporters('gulp-sass'))
+      }).on("error", reporters("gulp-sass"))
     )
     .pipe(
       $.autoprefixer({
-        browsers: ['last 2 versions']
+        browsers: ["last 2 versions"]
       })
     )
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('.tmp/styles'))
+    .pipe($.sourcemaps.write("."))
+    .pipe(gulp.dest(".tmp/styles"))
     // Concatenate and minify styles
-    .pipe($.if('*.css', $.minifyCss()))
+    .pipe($.if("*.css", $.minifyCss()))
     .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('dist/styles'))
-    .pipe(gulp.dest('app/styles'))
+    .pipe(gulp.dest("dist/styles"))
+    .pipe(gulp.dest("app/styles"))
     .pipe(
       $.size({
-        title: 'styles'
+        title: "styles"
       })
     )
-)
+);
 
 // Concatenate and minify JavaScript
-gulp.task('scripts', ['prettier-scripts'], () =>
+gulp.task("scripts", () =>
   gulp
     .src([
       // Note: Since we are not using useref in the scripts build pipeline,
       //       you need to explicitly list your scripts here in the right order
       //       to be correctly concatenated
-      './app/scripts/**/*.js'
+      "./app/scripts/**/*.js"
       // Other scripts
     ])
     .pipe($.sourcemaps.init())
-    .pipe($.concat('main.min.js'))
+    .pipe($.concat("main.min.js"))
     .pipe(
       $.babel(
         Object.assign(
           {
-            plugins: ['transform-es2015-modules-systemjs']
+            plugins: ["transform-es2015-modules-systemjs"]
           },
-          JSON.parse(fs.readFileSync('.babelrc', 'utf8'))
+          JSON.parse(fs.readFileSync(".babelrc", "utf8"))
         )
       )
     )
@@ -255,15 +254,15 @@ gulp.task('scripts', ['prettier-scripts'], () =>
     .pipe($.sourcemaps.write())
     .pipe(
       $.size({
-        title: 'scripts'
+        title: "scripts"
       })
     )
-    .pipe(gulp.dest('dist/scripts'))
-)
+    .pipe(gulp.dest("dist/scripts"))
+);
 
-gulp.task('prettier-html', () =>
+gulp.task("prettier-html", () =>
   gulp
-    .src(['app/*.html', 'app/**/*.html'])
+    .src(["app/*.html", "app/**/*.html"])
     .pipe(
       $.prettier({
         singleQuote: true,
@@ -272,15 +271,15 @@ gulp.task('prettier-html', () =>
       })
     )
     .pipe(gulp.dest(({ base }) => base))
-)
+);
 
 // Scan your HTML for assets & optimize them
-gulp.task('html', ['prettier-html'], () =>
+gulp.task("html", ["prettier-html"], () =>
   gulp
-    .src('app/**/*.html')
+    .src("app/**/*.html")
     .pipe(
       $.useref({
-        searchPath: '{.tmp,app}'
+        searchPath: "{.tmp,app}"
       })
     )
     // Remove any unused CSS
@@ -288,9 +287,9 @@ gulp.task('html', ['prettier-html'], () =>
     //       the next line to only include styles your project uses.
     .pipe(
       $.if(
-        '*.css',
+        "*.css",
         $.uncss({
-          html: ['app/index.html', 'app/resume.html'],
+          html: ["app/index.html", "app/resume.html"],
           // CSS Selectors for UnCSS to ignore
           ignore: []
         })
@@ -298,105 +297,91 @@ gulp.task('html', ['prettier-html'], () =>
     )
     // Concatenate and minify styles
     // In case you are still using useref build blocks
-    .pipe($.if('*.css', $.minifyCss()))
+    .pipe($.if("*.css", $.minifyCss()))
 
     // Minify any HTML
-    .pipe($.if('*.html', $.minifyHtml()))
+    .pipe($.if("*.html", $.minifyHtml()))
     // Output files
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest("dist"))
     .pipe(
       $.size({
-        title: 'html'
+        title: "html"
       })
     )
-)
+);
 
 // Clean output directory
-gulp.task('clean', cb =>
+gulp.task("clean", cb =>
   del(
-    ['.tmp', '.publish', 'dist/*', '!dist/.git'],
+    [".tmp", ".publish", "dist/*", "!dist/.git"],
     {
       dot: true
     },
     cb
   )
-)
+);
 
 // Watch files for changes & reload
-gulp.task('start', ['styles'], () => {
+gulp.task("start", ["styles"], () => {
   browserSync({
     open: false,
     notify: true,
     // Customize the BrowserSync console logging prefix
-    logPrefix: 'WSK',
+    logPrefix: "WSK",
     // Run as an https by uncommenting 'https: true'
     // Note: this uses an unsigned certificate which on first access
     //       will present a certificate warning in the browser.
     // https: true,
-    server: ['.tmp', 'app']
-  })
+    server: [".tmp", "app"]
+  });
 
-  gulp.watch(['app/*.html', 'app/**/*.html'], ['prettier-html'])
-  gulp.watch(['app/**/*.html'], reload)
-  gulp.watch(['app/styles/**/*.{scss,css}'], ['styles'])
-  gulp.watch(['app/styles/**/*.{scss,css}'], reload)
-  gulp.watch(['app/resume.html'], ['resume', 'resume-doc', 'prettier-html'])
-  gulp.watch(['app/images/**/*'], reload)
-})
+  gulp.watch(["app/*.html", "app/**/*.html"], ["prettier-html"]);
+  gulp.watch(["app/**/*.html"], reload);
+  gulp.watch(["app/styles/**/*.{scss,css}"], ["styles"]);
+  gulp.watch(["app/styles/**/*.{scss,css}"], reload);
+  gulp.watch(["app/resume.html"], ["resume", "resume-doc", "prettier-html"]);
+  gulp.watch(["app/images/**/*"], reload);
+});
 
 // Build and serve the output from the dist build
-gulp.task('start:dist', ['default'], () =>
+gulp.task("start:dist", ["default"], () =>
   browserSync({
     notify: false,
-    logPrefix: 'WSK',
+    logPrefix: "WSK",
     // Run as an https by uncommenting 'https: true'
     // Note: this uses an unsigned certificate which on first access
     //       will present a certificate warning in the browser.
     // https: true,
-    server: 'dist',
-    baseDir: 'dist',
+    server: "dist",
+    baseDir: "dist",
     open: false
   })
-)
+);
 
 // Build production files, the default task
-gulp.task('default', ['clean'], cb =>
+gulp.task("default", ["clean"], cb =>
   runSequence(
-    'vendor',
-    ['styles', 'resume', 'resume-doc'],
-    ['html', 'scripts', 'images', 'icons', 'fonts', 'copy'],
-    'generate-service-worker',
+    "vendor",
+    ["styles", "resume", "resume-doc"],
+    ["html", "scripts", "images", "icons", "fonts", "copy"],
+    "generate-service-worker",
     cb
   )
-)
-
-// Run PageSpeed Insights
-gulp.task('pagespeed', cb =>
-  pagespeed(
-    'furkantunali.com',
-    {
-      strategy: 'mobile'
-      // By default we use the PageSpeed Insights free (no API key) tier.
-      // Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
-      // key: 'YOUR_API_KEY'
-    },
-    cb
-  )
-)
+);
 
 // See http://www.html5rocks.com/en/tutorials/service-worker/introduction/ for
 // an in-depth explanation of what service workers are and why you should care.
 // Generate a service worker file that will provide offline functionality for
 // local resources. This should only be done for the 'dist' directory, to allow
 // live reload to work as expected when serving from the 'app' directory.
-gulp.task('generate-service-worker', cb => {
-  const rootDir = 'dist'
+gulp.task("generate-service-worker", cb => {
+  const rootDir = "dist";
 
-  let name = pkg.name || 'furkantunali.com'
-  let version = pkg.version || '0'
+  let name = pkg.name || "furkantunali.com";
+  let version = pkg.version || "0";
 
   swPrecache.write(
-    path.join(rootDir, 'service-worker.js'),
+    path.join(rootDir, "service-worker.js"),
     {
       // Used to avoid cache conflicts when serving on localhost.
       cacheId: `${name}=v${version}`,
@@ -419,12 +404,12 @@ gulp.task('generate-service-worker', cb => {
     },
     err => {
       if (err) {
-        cb(err)
-        return
+        cb(err);
+        return;
       }
-      cb()
+      cb();
     }
-  )
-})
+  );
+});
 
-$.npmScriptSync(gulp)
+$.npmScriptSync(gulp);
