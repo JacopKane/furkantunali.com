76e8409132775ad01b409e2af25187db3a00f657
Furkan Tunalı <mail@furkantunali.com>
2023-12-07 19:25:05 +0300

    initial firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,39 +1,19 @@
-import * as functions from 'firebase-functions'
-import {launch} from 'puppeteer-core'
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const downloadAsPDF = functions
-.region('europe-west1').https.onRequest(async (_request, response) => {
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
 
-  const browser = await launch({ headless: true })
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
 
-    try {
-
-      const page = await browser.newPage();
-      await page.goto('https://furkantunali-1043.web.app/resume-doc.html', { waitUntil: 'networkidle2' });
-
-
-      const buffer = await page.pdf({
-          format: "A4",
-          printBackground: true,
-          margin: {
-              left: '10px',
-              top: '20px',
-              right: '10px',
-              bottom: '20px'
-          }
-      })
-
-
-      response.header('Content-Type', 'application/pdf;charset=utf-8');
-      response.header('Content-Disposition', 'attachment; filename=Furkan_Tunali_Resume');
-      response.type('application/pdf').send(buffer);
-
-    } catch (e) {
-        response.status(500).send(e.toString());
-    }
-
-    await browser.close();
-});
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
