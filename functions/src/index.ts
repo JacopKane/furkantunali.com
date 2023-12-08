4569cec691641bdb75bc5c5511b42543e6070865
Furkan Tunalı <mail@furkantunali.com>
2023-12-08 15:37:10 +0300

    fix firebase deployment

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,17 +14,39 @@ import { launch } from "puppeteer";
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
+const runtimeOpts = {
+  timeoutSeconds: 300,
+  memory: "1GB",
+};
+
 export const downloadAsPDF = functions
+  .runWith(runtimeOpts)
   .region("europe-west1")
   .https.onRequest(async (_request, response) => {
     logger.info("Begin downloadAsPDF", { structuredData: true });
 
-    const browser = await launch({ headless: "chrome" });
+    const browser = await launch({
+      headless: true,
+      timeout: 20000,
+      ignoreHTTPSErrors: true,
+      slowMo: 0,
+      args: [
+        "--disable-gpu",
+        "--disable-dev-shm-usage",
+        "--disable-setuid-sandbox",
+        "--no-first-run",
+        "--no-sandbox",
+        "--no-zygote",
+        "--window-size=2480,3508",
+      ],
+    });
 
     logger.info("Browser launched", { structuredData: true });
 
     try {
       const page = await browser.newPage();
+      await page.setViewport({ width: 2480, height: 3508 });
+
       await page.goto("https://furkantunali-1043.web.app/resume-doc.html", {
         waitUntil: "networkidle2",
       });
